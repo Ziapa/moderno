d2f4645b48a766012bb5fd5096d3e588866dacba
Ziapa <SmiT4eggg@bk.ru>
2020-02-10 15:13:32 +0300

    add header

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ let gulp = require("gulp"),
     
 
 gulp.task("sass", function(){
-    return gulp.src("app/scss/style.scss")
+    return gulp.src("app/scss/**/*.scss")
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(rename({suffix : '.min'}))
         .pipe(autoprefixer({
@@ -23,8 +23,8 @@ gulp.task("sass", function(){
 gulp.task("style", function(){
     return gulp.src([
         "node_modules/normalize.css/normalize.css",
-        "node_modules/slick-carousel/slick/slick.js",
-        "node_modules/magnific-popup/dist/jquery.magnific-popup.js"
+        "node_modules/slick-carousel/slick/slick.css",
+        "node_modules/magnific-popup/dist/magnific-popup.css"
     ])
             .pipe(concat("libs.min.css"))
             .pipe(cssmin())
@@ -33,8 +33,8 @@ gulp.task("style", function(){
 
 gulp.task("script", function(){
     return gulp.src([
-        "node_modules/slick-carousel/slick/slick.css",
-        "node_modules/magnific-popup/dist/jquery.magnific-popup.css"
+        "node_modules/slick-carousel/slick/slick.js",
+        "node_modules/magnific-popup/dist/jquery.magnific-popup.js"
     ])
             .pipe(concat("libs.min.js"))
             .pipe(uglify())
@@ -60,9 +60,9 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task("watch", function(){
-    gulp.watch("app/scss/style.scss", gulp.parallel("sass"))
-    gulp.watch("app/*html", gulp.parallel("html"))
-    gulp.watch("app/js/*js", gulp.parallel("js"))
+    gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"))
+    gulp.watch("app/*.html", gulp.parallel("html"))
+    gulp.watch("app/js/*.js", gulp.parallel("js"))
 })
 
 gulp.task("default", gulp.parallel("style", "script", "sass", "watch", "browser-sync"))
