33936f2bb63095f83d5b4dfbd5f91150b11a3829
Ziapa <SmiT4eggg@bk.ru>
2020-02-18 11:45:39 +0300

    end product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,24 @@
 $(function(){
 
+    $(".rate-stear").rateYo({
+        starWidth: "12px",
+        rating: 3.6,
+        readOnly: true
+      });
+
+      $(".product-slider__inner").slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false
+
+      });
+
     var mixer = mixitup(".products__inner-box");
+    
+
+
+
 
     
 })
