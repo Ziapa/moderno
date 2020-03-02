72c71cb876528defe3a86c9ff1f9d8ea7f73959f
Ziapa <SmiT4eggg@bk.ru>
2020-03-02 15:28:24 +0300

    added aside

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,33 @@
-$(function(){
+$(function () {
 
-    $(".rate-stear").rateYo({
-        starWidth: "12px",
-        rating: 3.6,
-        readOnly: true
-      });
+  $(".rate-stear").rateYo({
+    starWidth: "12px",
+    rating: 3.6,
+    readOnly: true
+  });
 
-      $(".product-slider__inner").slick({
-        dots: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        arrows: false
+  $(".product-slider__inner").slick({
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false
 
-      });
+  });
 
-    var mixer = mixitup(".products__inner-box");
-    
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+
+  var mixer = mixitup(".products__inner-box");
 
 
 
 
-    
+
+
 })
